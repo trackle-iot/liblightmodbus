f81f017aa0260f7a746dbd12304afcaacf393a86
Jacajack <Jacajack@wp.pl>
2016-03-08 22:01:57 +0100

    Add code for processing request16

--- slave.c
+++ slave.c
@@ -13,8 +13,6 @@ void MODBUSRequest03( union MODBUSParser *Parser )
 	//Check frame CRC
 	if ( MODBUSCRC16( ( *Parser ).Frame, FrameLength - 2 ) != ( *Parser ).Request03.CRC ) return; //EXCEPTION (in future)
 
-	printf( "CRC is ok\n" ); //*DEBUG*
-
 	//Swap endianness of longer members (but not CRC)
 	( *Parser ).Request03.FirstRegister = MODBUSSwapEndian( ( *Parser ).Request03.FirstRegister );
 	( *Parser ).Request03.RegisterCount = MODBUSSwapEndian( ( *Parser ).Request03.RegisterCount );
@@ -23,6 +21,7 @@ void MODBUSRequest03( union MODBUSParser *Parser )
 	if ( ( *Parser ).Request03.FirstRegister >= MODBUSRegisterCount || ( *Parser ).Request03.FirstRegister + ( *Parser ).Request03.RegisterCount > MODBUSRegisterCount ) return; //EXCEPTION (in future)
 
 	//FORMAT RESPONSE HERE
+	printf( "OK\n" ); //*DEBUG*
 }
 
 void MODBUSRequest06( union MODBUSParser *Parser )
@@ -38,12 +37,42 @@ void MODBUSRequest06( union MODBUSParser *Parser )
 	( *Parser ).Request06.Value = MODBUSSwapEndian( ( *Parser ).Request06.Value );
 
 	//Check if register is in valid range
-	if ( ( *Parser ).Request06.Register >= MODBUSRegisterCount ); //EXCEPTION (in future)
+	if ( ( *Parser ).Request06.Register >= MODBUSRegisterCount ) return; //EXCEPTION (in future)
 
 	//Write register
 	MODBUSRegisters[( *Parser ).Request06.Register] = ( *Parser ).Request06.Value;
 
 	//FORMAT RESPONSE HERE
+	printf( "OK\n" ); //*DEBUG*
+}
+
+void MODBUSRequest16( union MODBUSParser *Parser )
+{
+	//Update frame length
+	uint8_t i = 0;
+	uint8_t FrameLength = 9 + ( *Parser ).Request16.BytesCount;
+
+	//Check if bytes or registers count isn't 0
+	if ( ( *Parser ).Request16.BytesCount == 0 || ( *Parser ).Request16.RegisterCount == 0 ) return; //EXCEPTION (in future)
+
+	//Check frame CRC
+	//Shifting is used instead of dividing for optimisation on smaller devices (AVR)
+	if ( MODBUSCRC16( ( *Parser ).Frame, FrameLength - 2 ) != ( *Parser ).Request16.Values[( *Parser ).Request16.BytesCount >> 1] ) return; //EXCEPTION (in future)
+
+	//Swap endianness of longer members (but not CRC)
+	( *Parser ).Request16.FirstRegister = MODBUSSwapEndian( ( *Parser ).Request16.FirstRegister );
+	( *Parser ).Request16.RegisterCount = MODBUSSwapEndian( ( *Parser ).Request16.RegisterCount );
+
+	//Check if register are in valid range
+	if ( ( *Parser ).Request16.RegisterCount != ( ( *Parser ).Request16.BytesCount >> 1 ) ) return; //EXCEPTION (in future)
+	if ( ( *Parser ).Request16.FirstRegister >= MODBUSRegisterCount || ( *Parser ).Request16.FirstRegister + ( *Parser ).Request16.RegisterCount > MODBUSRegisterCount ) return; //EXCEPTION (in future)
+
+	//Write values to registers
+	for ( i = 0; i < ( *Parser ).Request16.RegisterCount; i++ )
+		MODBUSRegisters[( *Parser ).Request16.FirstRegister + i] = ( *Parser ).Request16.Values[i];
+
+	//FORMAT RESPONSE HERE
+	printf( "OK\n" ); //*DEBUG*
 }
 
 void MODBUSParseRequest( uint8_t *Frame, uint8_t FrameLength )
@@ -70,6 +99,7 @@ void MODBUSParseRequest( uint8_t *Frame, uint8_t FrameLength )
 			break;
 
 		case 16: //Write multiple holding registers
+			MODBUSRequest16( &Parser );
 			break;
 	}
 }

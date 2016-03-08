02224e5a55fcfeca687508fb21a98df16db2f349
Jacajack <Jacajack@wp.pl>
2016-03-08 19:12:48 +0100

    Add extern variables declarations

--- slave.h
+++ slave.h
@@ -1,6 +1,10 @@
 #include "modlib.h"
 #include "parser.h"
+
 //Variables definitions
+extern uint8_t MODBUSAddress; //Address of device
+extern uint16_t *MODBUSRegisters; //Pointer to slave-side modbus registers
+extern uint16_t MODBUSRegisterCount; //Count of slave-side modbus registers
 
 //Function prototypes
-extern void MODBUSParseRequest( uint8_t *, uint8_t );
+extern void MODBUSParseRequest( uint8_t *, uint8_t ); //Parse and interpret given modbus frame on slave-side

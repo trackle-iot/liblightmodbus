d58d8dad3a40fd318d8464cdf4bbbd757d65f9f2
Jacajack <Jacajack@wp.pl>
2016-03-08 15:28:55 +0100

    Add parser union (incomplete)

--- parser.h
+++ parser.h
@@ -1,4 +1,68 @@
+//This parser is incomplete yet!
+
 union MODBUSParser
 {
-	
+    char Frame[256];
+
+    struct __attribute__( ( __packed__ ) )
+    {
+        uint8_t Address;
+        uint8_t Function;
+    } Base; //Base shared bytes, which have always same meaning
+
+    struct __attribute__( ( __packed__ ) )
+    {
+        uint8_t Address;
+        uint8_t Function;
+        uint16_t FirstRegister;
+        uint16_t RegisterCount;
+        uint16_t CRC;
+    } Request03; //Read multiple holding register
+
+    struct __attribute__( ( __packed__ ) )
+    {
+        uint8_t Address;
+        uint8_t Function;
+        uint8_t BytesCount;
+        uint16_t Values[128];
+        uint16_t CRC;
+    } Response03; //Read multiple holding registers - response
+
+    struct __attribute__( ( __packed__ ) )
+    {
+        uint8_t Address;
+        uint8_t Function;
+        uint16_t Register;
+        uint16_t Value;
+        uint16_t CRC;
+    } Request06; //Write single holding register
+
+    struct __attribute__( ( __packed__ ) )
+    {
+        uint8_t Address;
+        uint8_t Function;
+        uint16_t Register;
+        uint16_t Value;
+        uint16_t CRC;
+    } Response06; //Write single holding register
+
+    struct __attribute__( ( __packed__ ) )
+    {
+        uint8_t Address;
+        uint8_t Function;
+        uint16_t FirstRegister;
+        uint16_t RegisterCount;
+        uint8_t BytesCount;
+        uint16_t Values[128];
+        uint16_t CRC;
+    } Request16; //Write single holding register
+
+    struct __attribute__( ( __packed__ ) )
+    {
+        uint8_t Address;
+        uint8_t Function;
+        uint16_t FirstRegister;
+        uint16_t RegisterCount;
+        uint16_t CRC;
+    } Response16; //Write single holding register
 };

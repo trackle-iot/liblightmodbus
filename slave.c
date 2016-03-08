2b88c338ee69373b96925321530353be106b085e
Jacajack <Jacajack@wp.pl>
2016-03-08 19:03:30 +0100

    Correct typos

--- slave.c
+++ slave.c
@@ -1,1 +1,37 @@
 #include "slave.h"
+
+uint8_t MODBUSAddress; //Address of device
+uint16_t *MODBUSRegisters; //Pointer to slave-side modbus registers
+uint16_t MODBUSRegisterCout; //Count of slave-side modbus registers
+
+void MODBUSParseRequest( uint8_t *Frame, uint8_t FrameLength )
+{
+	//Init parser union
+	union MODBUSParser Parser;
+	memcpy( Parser.Frame, Frame, FrameLength );
+
+	//User needs to free memory alocated for frame himself!
+
+	//If frame is not broadcasted and address doesn't match skip parsing
+	if ( Parser.Base.Address != MODBUSAddress && Parser.Base.Address != 0 ) return;
+
+	switch( Parser.Base.Function )
+	{
+		case 3: //Read multiple holding register
+
+			//Check frame CRC
+			if ( MODBUSCRC16( Parser.Frame, 6 ) != Parser.Request03.CRC ) return; //EXCEPTION (in future)
+
+			//Swap endianness of longer members (but not CRC)
+			Parser.Request03.FirstRegister = MODBUSSwapEndian( Parser.Request03.FirstRegister );
+			Parser.Request03.RegisterCount = MODBUSSwapEndian( Parser.Request03.RegisterCount );
+
+			break;
+
+		case 6: //Write single holding register
+			break;
+
+		case 16: //Write multiple holding registers
+			break;
+	}
+}

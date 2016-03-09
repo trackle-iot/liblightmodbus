3d548f2142935a705a848cab89e023c787a7d30b
Jacajack <Jacajack@wp.pl>
2016-03-09 15:49:10 +0100

    Memory for union is now allocated using malloc( ) It's unsafe when bad values are typed in, but it also works

--- slave.c
+++ slave.c
@@ -5,8 +5,16 @@ uint8_t MODBUSAddress; //Address of device
 uint16_t *MODBUSRegisters; //Pointer to slave-side modbus registers
 uint16_t MODBUSRegisterCount; //Count of slave-side modbus registers
 
+void MODBUSException( uint8_t ExceptionCode, uint8_t *Frame )
+{
+
+}
+
 void MODBUSRequest03( union MODBUSParser *Parser )
 {
+	//Read multiple holding registers
+	//Using data from union pointer
+
 	//Update frame length
 	uint8_t FrameLength = 8;
 
@@ -26,6 +34,9 @@ void MODBUSRequest03( union MODBUSParser *Parser )
 
 void MODBUSRequest06( union MODBUSParser *Parser )
 {
+	//Write single holding register
+	//Using data from union pointer
+
 	//Update frame length
 	uint8_t FrameLength = 8;
 
@@ -48,6 +59,9 @@ void MODBUSRequest06( union MODBUSParser *Parser )
 
 void MODBUSRequest16( union MODBUSParser *Parser )
 {
+	//Write multiple holding registers
+	//Using data from union pointer
+
 	//Update frame length
 	uint8_t i = 0;
 	uint8_t FrameLength = 9 + ( *Parser ).Request16.BytesCount;
@@ -80,26 +94,31 @@ void MODBUSParseRequest( uint8_t *Frame, uint8_t FrameLength )
 	//Parse and interpret given modbus frame on slave-side
 
 	//Init parser union
-	union MODBUSParser Parser;
-	memcpy( Parser.Frame, Frame, FrameLength );
-
+	//This one is actually unsafe, so it's easy to create a segmentation fault, so be careful here
+	//Allowable frame array size in union is 256, but here I'm only allocating amount of frame length
+	//It is even worse, compiler won't warn you, when you are outside the range
+	//It works, and it uses much less memory, so I guess a bit of risk is fine in this case
+	//If something goes wrong, this can be changed back
+	union MODBUSParser *Parser;
+	Parser = malloc( FrameLength );
+	memcpy( ( *Parser ).Frame, Frame, FrameLength );
 	//User needs to free memory alocated for frame himself!
 
 	//If frame is not broadcasted and address doesn't match skip parsing
-	if ( Parser.Base.Address != MODBUSAddress && Parser.Base.Address != 0 ) return;
+	if ( ( *Parser ).Base.Address != MODBUSAddress && ( *Parser ).Base.Address != 0 ) return;
 
-	switch( Parser.Base.Function )
+	switch( ( *Parser ).Base.Function )
 	{
-		case 3: //Read multiple holding register
-			MODBUSRequest03( &Parser );
+		case 3: //Read multiple holding registers
+			MODBUSRequest03( Parser );
 			break;
 
 		case 6: //Write single holding register
-			MODBUSRequest06( &Parser );
+			MODBUSRequest06( Parser );
 			break;
 
 		case 16: //Write multiple holding registers
-			MODBUSRequest16( &Parser );
+			MODBUSRequest16( Parser );
 			break;
 	}
 }

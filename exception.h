2fc051c668c94152335ad2fe049e7a9184c4dfb6
Jacajack <Jacajack@wp.pl>
2016-03-09 16:01:45 +0100

    Add exception union file

---
+++ exception.h
@@ -0,0 +1,13 @@
+//Exception generator union
+
+union MODBUSException
+{
+	uint8_t Frame[5];
+	struct __attribute__( ( __packed__ ) )
+	{
+		uint8_t Address;
+		uint8_t Function;
+		uint8_t ExceptionCode;
+		uint16_t CRC;
+	} Exception;
+};

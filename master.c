e16b6dec212a86fb65db026ddcfcdd89efd30ec5
Jacajack <Jacajack@wp.pl>
2016-03-22 18:36:33 +0100

    Add exceptions parsing (not tested yet)

--- master.c
+++ master.c
@@ -3,6 +3,31 @@
 //Master configurations
 MODBUSMasterStatus MODBUSMaster;
 
+void MODBUSParseException( union MODBUSParser *Parser )
+{
+	//Parse exception frame and write data to MODBUSMaster structure
+
+	//Allocate memory for exception parser
+	union MODBUSException *Exception = malloc( sizeof( union MODBUSException ) );
+	memcpy( ( *Exception ).Frame, ( *Parser ).Frame, sizeof( union MODBUSException ) );
+
+	//Check CRC
+	if ( MODBUSCRC16( ( *Exception ).Frame, 3 ) != ( *Exception ).Exception.CRC )
+	{
+		free( Exception );
+		return;
+	}
+
+	//Copy data
+	MODBUSMaster.Exception.Address = ( *Exception ).Exception.Address;
+	MODBUSMaster.Exception.Function = ( *Exception ).Exception.Function;
+	MODBUSMaster.Exception.Code = ( *Exception ).Exception.ExceptionCode;
+
+	MODBUSMaster.Error = 1;
+
+	free( Exception );
+}
+
 void MODBUSParseResponse( uint8_t *Frame, uint8_t FrameLength )
 {
 	//This function parses response from master
@@ -12,8 +37,16 @@ void MODBUSParseResponse( uint8_t *Frame, uint8_t FrameLength )
 	union MODBUSParser *Parser = malloc( FrameLength );
 	memcpy( ( *Parser ).Frame,  Frame, FrameLength );
 
-	if ( MODBUS_MASTER_BASIC )
-		MODBUSParseResponseBasic( Parser );
+	//Check if frame is exception response
+	if ( ( *Parser ).Base.Function & 128 )
+	{
+		MODBUSParseException( Parser );
+	}
+	else
+	{
+		if ( MODBUS_MASTER_BASIC )
+			MODBUSParseResponseBasic( Parser );
+	}
 
 	//Free used memory
 	free( Parser );

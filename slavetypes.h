6634d796894e48cdd9247687eb7edc233fb77d23
Jacajack <Jacajack@wp.pl>
2016-03-22 14:18:10 +0100

    Move types declarations to new file

---
+++ slavetypes.h
@@ -0,0 +1,13 @@
+typedef struct
+{
+	uint8_t Length; //Length of frame, if it's equal to 0, frame is not ready
+	uint8_t *Frame; //Response frame content
+} MODBUSResponseStatus; //Type containing information about frame that is set up at slave side
+
+typedef struct
+{
+	uint8_t Address;
+	uint16_t *Registers;
+	uint16_t RegisterCount;
+	MODBUSResponseStatus Response;
+} MODBUSSlaveStatus; //Type containing slave device configuration data

08d13074df1b7cd47562c53f1797e3a927108653
Jacajack <Jacajack@wp.pl>
2016-03-26 15:15:35 +0100

    New test procedure

--- test/test.c
+++ test/test.c
@@ -6,12 +6,6 @@ In future, when request-creating functions are implemented, this will test much 
 */
 
 uint16_t Registers[8] = { 0, 1, 2, 3, 4, 5, 6, 7 };
-//uint8_t Frame[256] = { 0x20, 0x10, 0x00, 0x03, 0x00, 0x02, 0x04, 0xFA, 0xFB, 0xEA, 0xEB }; //R16
-//uint8_t FrameLength = 11; //R16
-//uint8_t Frame[256] = { 0x20, 0x03, 0x00, 0x02, 0x00, 0x04 }; //R03
-//uint8_t FrameLength = 6; //R03
-//uint8_t Frame[256] = { 0x20, 0x06, 0x00, 0x02, 0x00, 0x04 }; //R06
-//uint8_t FrameLength = 6; //R06
 
 void TermRGB( unsigned char R, unsigned char G, unsigned char B )
 {
@@ -19,62 +13,52 @@ void TermRGB( unsigned char R, unsigned char G, unsigned char B )
     printf( "\033[38;5;%dm", 16 + B + G * 6 + R * 36 );
 }
 
-int main( )
+void Test( )
 {
-	unsigned char i = 0;
+  uint8_t i = 0;
 
-	TermRGB( 4, 1, 0 );
-	printf( "\nInit...\n" );
+  //Clear registers
+  memset( Registers, 0, 8 * 2 );
 
-	//Init slave-side
-	MODBUSSlave.Registers = Registers;
-	MODBUSSlave.RegisterCount = 8;
-	MODBUSSlaveInit( 32, MODBUSSlave.Registers, MODBUSSlave.RegisterCount );
-	MODBUSMasterInit( );
-
+  //Start test
 	TermRGB( 4, 2, 0 );
 	printf( "Test started...\n" );
 
-  /*
-	TermRGB( 3, 3, 0 );
-	printf( "Calculate CRC of given frame... %x\n", MODBUSCRC16( Frame, FrameLength ) );
-	Frame[FrameLength] = MODBUSCRC16( Frame, FrameLength ) & 0xFF;
-	Frame[FrameLength + 1] = ( MODBUSCRC16( Frame, FrameLength ) & 0xFF00 ) >> 8;
-
+  //Dump frame
 	TermRGB( 2, 4, 0 );
 	printf( "Dump the frame:\n" );
-	for ( i = 0; i < FrameLength + 2; i++ )
-		printf( "%x%s", Frame[i], ( i == FrameLength + 1 ) ? "\n" : ", " );
-  */
+	for ( i = 0; i < MODBUSMaster.Request.Length; i++ )
+		printf( "%.2x%s", MODBUSMaster.Request.Frame[i], ( i == MODBUSMaster.Request.Length - 1 ) ? "\n" : "-" );
 
-  MODBUSBuildRequest06( 0x20, 0x04, 0x54 );
-
+  //Dump slave registers
 	TermRGB( 2, 4, 2 );
 	printf( "Dump registers:\n" );
 	for ( i = 0; i < MODBUSSlave.RegisterCount; i++ )
-		printf( "%x%s", Registers[i], ( i == MODBUSSlave.RegisterCount - 1 ) ? "\n" : ", " );
+		printf( "0x%.2x%s", Registers[i], ( i == MODBUSSlave.RegisterCount - 1 ) ? "\n" : ", " );
 
+  //Parse request
 	TermRGB( 1, 4, 3 );
 	printf( "Let slave parse frame...\n" );
-
-	TermRGB( 0, 3, 4 );
 	MODBUSParseRequest( MODBUSMaster.Request.Frame, MODBUSMaster.Request.Length );
 
+  //Dump slave registers
 	TermRGB( 0, 2, 4 );
 	printf( "Dump registers:\n" );
 	for ( i = 0; i < MODBUSSlave.RegisterCount; i++ )
-		printf( "%x%s", Registers[i], ( i == MODBUSSlave.RegisterCount - 1 ) ? "\n" : ", " );
+		printf( "0x%.2x%s", Registers[i], ( i == MODBUSSlave.RegisterCount - 1 ) ? "\n" : ", " );
 
+  //Dump response
 	TermRGB( 0, 1, 4 );
 	printf( "Dump response - length = %d:\n", MODBUSSlave.Response.Length );
 	for ( i = 0; i < MODBUSSlave.Response.Length; i++ )
 		printf( "%x%s", MODBUSSlave.Response.Frame[i], ( i == MODBUSSlave.Response.Length - 1 ) ? "\n" : ", " );
 
+  //Process response
 	TermRGB( 0, 2, 4 );
 	printf( "Let master process response...\n" );
-
 	MODBUSParseResponse( MODBUSSlave.Response.Frame, MODBUSSlave.Response.Length, MODBUSMaster.Request.Frame, MODBUSMaster.Request.Length );
 
+  //Dump parsed data
 	TermRGB( 0, 3, 4 );
 	printf( "Error - %d\n\rFinished - %d\n\r", MODBUSMaster.Error, MODBUSMaster.Finished );
 	for ( i = 0; i < MODBUSMaster.DataLength; i++ )
@@ -86,6 +70,37 @@ int main( )
 		TermRGB( 4, 1, 0 );
 		printf( "\t - ex addr: 0x%x, fun: 0x%x, code: 0x%x\n\r", MODBUSMaster.Exception.Address, MODBUSMaster.Exception.Function, MODBUSMaster.Exception.Code );
 	}
+  printf( "\x1b[0m----------------------------------------\n" );
+}
+
+int main( )
+{
+	TermRGB( 4, 1, 0 );
+	printf( "\nInit...\n" );
+
+	//Init slave and master
+	MODBUSSlaveInit( 32, Registers, 8 );
+	MODBUSMasterInit( );
+
+  //Request06 - ok
+  printf( "\x1b[0mCorrect request...\n" );
+  MODBUSBuildRequest06( 0x20, 0x06, 0xf6 );
+  Test( );
+
+  //Request06 - bad register
+  printf( "\x1b[0mBad register...\n" );
+  MODBUSBuildRequest06( 0x20, 0xf6, 0xf6 );
+  Test( );
+
+  //Request06 - broadcast
+  printf( "\x1b[0mBroadcast...\n" );
+  MODBUSBuildRequest06( 0x00, 0x06, 0xf6 );
+  Test( );
+
+  //Request06 - other slave address
+  printf( "\x1b[0mOther address...\n" );
+  MODBUSBuildRequest06( 0x10, 0x06, 0xf6 );
+  Test( );
 
 	//Reset terminal colors
 	printf( "\x1b[0m" );

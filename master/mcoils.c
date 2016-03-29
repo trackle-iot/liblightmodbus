77a691c9798a4f4991a58c98e2776314907d6d08
Jacajack <Jacajack@wp.pl>
2016-03-30 00:10:40 +0200

    Add function for building request15

--- master/mcoils.c
+++ master/mcoils.c
@@ -45,7 +45,7 @@ uint8_t MODBUSBuildRequest01( uint8_t Address, uint16_t FirstCoil, uint16_t Coil
 
 uint8_t MODBUSBuildRequest05( uint8_t Address, uint16_t Coil, uint16_t Value )
 {
-	//Build request01 frame, to send it so slave
+	//Build request05 frame, to send it so slave
 	//Write single coil
 
 	//Set frame length
@@ -69,6 +69,49 @@ uint8_t MODBUSBuildRequest05( uint8_t Address, uint16_t Coil, uint16_t Value )
 
 	//Calculate CRC
 	( *Builder ).Request01.CRC = MODBUSCRC16( ( *Builder ).Frame, FrameLength - 2 );
+
+	//Copy frame from builder to output structure
+	memcpy( MODBUSMaster.Request.Frame, ( *Builder ).Frame, FrameLength );
+
+	//Free used memory
+	free( Builder );
+
+	MODBUSMaster.Request.Length = FrameLength;
+
+	return 0;
+}
+
+uint8_t MODBUSBuildRequest15( uint8_t Address, uint16_t FirstCoil, uint16_t CoilCount, uint8_t *Values )
+{
+	//Build request15 frame, to send it so slave
+	//Write multiple coils
+
+	//Set frame length
+	uint8_t FrameLength = 10 + ( CoilCount >> 3 );
+	uint8_t i = 0;
+
+	//Set output frame length to 0 (in case of interrupts)
+	MODBUSMaster.Request.Length = 0;
+
+	if ( CoilCount > 256 ) return 1;
+
+	//Allocate memory for frame builder
+	union MODBUSParser *Builder = (union MODBUSParser *) malloc( FrameLength );
+
+	//Reallocate memory for final frame
+	MODBUSMaster.Request.Frame = (uint8_t *) realloc( MODBUSMaster.Request.Frame, FrameLength );
+
+	( *Builder ).Base.Address = Address;
+	( *Builder ).Base.Function = 15;
+	( *Builder ).Request15.FirstCoil = MODBUSSwapEndian( FirstCoil );
+	( *Builder ).Request15.CoilCount = MODBUSSwapEndian( CoilCount );
+	( *Builder ).Request15.BytesCount = 1 + ( CoilCount >> 3 );
+
+	for ( i = 0; i < ( i + ( CoilCount >> 3 ) ); i++ )
+		( *Builder ).Request15.Values[i] = Values[i];
+
+	( *Builder ).Frame[FrameLength - 2] = MODBUSCRC16( ( *Builder ).Frame, FrameLength - 2 ) & 0x00FF;
+	( *Builder ).Frame[FrameLength - 1] = ( MODBUSCRC16( ( *Builder ).Frame, FrameLength - 2 ) & 0xFF00 ) >> 8;
 
 	//Copy frame from builder to output structure
 	memcpy( MODBUSMaster.Request.Frame, ( *Builder ).Frame, FrameLength );
--- master/mcoils.h
+++ master/mcoils.h
@@ -5,7 +5,7 @@
 //Functions for building requests
 extern uint8_t MODBUSBuildRequest01( uint8_t, uint16_t, uint16_t );
 extern uint8_t MODBUSBuildRequest05( uint8_t, uint16_t, uint16_t );
-//extern uint8_t MODBUSBuildRequest15( uint8_t, uint16_t, uint16_t, uint16_t * );
+extern uint8_t MODBUSBuildRequest15( uint8_t, uint16_t, uint16_t, uint8_t * );
 
 //Functions for parsing responses
 //extern void MODBUSParseResponse01( union MODBUSParser *, union MODBUSParser * );

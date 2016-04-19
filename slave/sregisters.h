24ccdf6f2a0802a286416133d31364d42d18d720
Jacajack <Jacajack@wp.pl>
2016-04-19 12:48:32 +0200

    Add prototypes of response-building functions

--- slave/sregisters.h
+++ slave/sregisters.h
@@ -9,3 +9,8 @@ extern void MODBUSBuildException( uint8_t, uint8_t );
 extern void MODBUSParseRequest03( union MODBUSParser *Parser );
 extern void MODBUSParseRequest06( union MODBUSParser *Parser );
 extern void MODBUSParseRequest16( union MODBUSParser *Parser );
+
+//Functions for building responses
+extern void MODBUSBuildResponse03( union MODBUSParser *Parser );
+extern void MODBUSBuildResponse06( union MODBUSParser *Parser );
+extern void MODBUSBuildResponse16( union MODBUSParser *Parser );

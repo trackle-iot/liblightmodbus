4ee20e213b952faeaf1beb08b8f78600f6c79f1c
Jacajack <Jacajack@wp.pl>
2016-04-25 20:01:33 +0200

    Add prototypes for functions for parsing slave's responses

--- master/mcoils.h
+++ master/mcoils.h
@@ -9,5 +9,5 @@ extern uint8_t MODBUSBuildRequest15( uint8_t, uint16_t, uint16_t, uint8_t * );
 
 //Functions for parsing responses
 extern void MODBUSParseResponse01( union MODBUSParser *, union MODBUSParser * );
-//extern void MODBUSParseResponse05( union MODBUSParser *, union MODBUSParser * );
-//extern void MODBUSParseResponse15( union MODBUSParser *, union MODBUSParser * );
+extern void MODBUSParseResponse05( union MODBUSParser *, union MODBUSParser * );
+extern void MODBUSParseResponse15( union MODBUSParser *, union MODBUSParser * );

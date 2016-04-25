585bc71ab63763bca82450012d79e88b5edf4a4a
Jacajack <Jacajack@wp.pl>
2016-04-25 20:00:02 +0200

    Add functions for parsing and responding for master request05. Also, fix exception function codes

--- slave/scoils.c
+++ slave/scoils.c
@@ -18,7 +18,7 @@ void MODBUSBuildResponse01( union MODBUSParser *Parser )
 
 	union MODBUSParser *Builder = (union MODBUSParser *) malloc( FrameLength ); //Allocate memory for builder union
 	memset( ( *Builder ).Frame, 0, FrameLength ); //Fill frame with zeros
-	
+
 	MODBUSSlave.Response.Frame = (uint8_t *) realloc( MODBUSSlave.Response.Frame, FrameLength ); //Reallocate response frame memory to needed memory
 	memset( MODBUSSlave.Response.Frame, 0, FrameLength ); //Empty response frame
 
@@ -45,6 +45,39 @@ void MODBUSBuildResponse01( union MODBUSParser *Parser )
 	free( Builder );
 }
 
+void MODBUSBuildResponse05( union MODBUSParser *Parser )
+{
+	//Response for master request05
+	uint8_t FrameLength = 8;
+
+	//Do not respond when frame is broadcasted
+	if ( ( *Parser ).Base.Address == 0 ) return;
+
+	union MODBUSParser *Builder = (union MODBUSParser *) malloc( FrameLength ); //Allocate memory for builder union
+	memset( ( *Builder ).Frame, 0, FrameLength ); //Fill frame with zeros
+
+	MODBUSSlave.Response.Frame = (uint8_t *) realloc( MODBUSSlave.Response.Frame, FrameLength ); //Reallocate response frame memory to needed memory
+	memset( MODBUSSlave.Response.Frame, 0, FrameLength ); //Empty response frame
+
+	//Set up basic response data
+	( *Builder ).Response05.Address = MODBUSSlave.Address;
+	( *Builder ).Response05.Function = ( *Parser ).Request03.Function;
+	( *Builder ).Response05.Coil = MODBUSSwapEndian( ( *Parser ).Request05.Coil );
+	( *Builder ).Response05.Value = MODBUSSwapEndian( ( *Parser ).Request05.Value );
+
+	//Calculate CRC
+	( *Builder ).Response05.CRC = MODBUSCRC16( ( *Builder ).Frame, FrameLength - 2 );
+
+	//Copy result from union to frame pointer
+	memcpy( MODBUSSlave.Response.Frame, ( *Builder ).Frame, FrameLength );
+
+	//Set frame length - frame is ready
+	MODBUSSlave.Response.Length = FrameLength;
+
+	//Free union memory
+	free( Builder );
+}
+
 void MODBUSParseRequest01( union MODBUSParser *Parser )
 {
 	//Read multiple coils
@@ -60,28 +93,65 @@ void MODBUSParseRequest01( union MODBUSParser *Parser )
 	( *Parser ).Request01.FirstCoil = MODBUSSwapEndian( ( *Parser ).Request01.FirstCoil );
 	( *Parser ).Request01.CoilCount = MODBUSSwapEndian( ( *Parser ).Request01.CoilCount );
 
-	//Check if register is in valid range
+	//Check if coil is in valid range
 	if ( ( *Parser ).Request01.CoilCount == 0 )
 	{
 		//Illegal data value error
-		if ( ( *Parser ).Base.Address != 0 ) MODBUSBuildException( 0x10, 0x03 );
+		if ( ( *Parser ).Base.Address != 0 ) MODBUSBuildException( 0x01, 0x03 );
 		return;
 	}
 
 	if ( ( *Parser ).Request01.CoilCount > MODBUSSlave.CoilCount )
 	{
 		//Illegal data address error
-		if ( ( *Parser ).Base.Address != 0 ) MODBUSBuildException( 0x10, 0x02 );
+		if ( ( *Parser ).Base.Address != 0 ) MODBUSBuildException( 0x01, 0x02 );
 		return;
 	}
 
 	if ( ( *Parser ).Request01.FirstCoil >= MODBUSSlave.CoilCount || (uint32_t) ( *Parser ).Request01.FirstCoil + (uint32_t) ( *Parser ).Request01.CoilCount > (uint32_t) MODBUSSlave.CoilCount )
 	{
 		//Illegal data address exception
-		if ( ( *Parser ).Base.Address != 0 ) MODBUSBuildException( 0x03, 0x02 );
+		if ( ( *Parser ).Base.Address != 0 ) MODBUSBuildException( 0x01, 0x02 );
 		return;
 	}
 
 	//Respond
 	MODBUSBuildResponse01( Parser );
+}
+
+void MODBUSParseRequest05( union MODBUSParser *Parser )
+{
+	//Write single coil
+	//Using data from union pointer
+
+	//Update frame length
+	uint8_t FrameLength = 8;
+
+	//Check frame CRC
+	if ( MODBUSCRC16( ( *Parser ).Frame, FrameLength - 2 ) != ( *Parser ).Request01.CRC ) return;
+
+	//Swap endianness of longer members (but not CRC)
+	( *Parser ).Request05.Coil = MODBUSSwapEndian( ( *Parser ).Request05.Coil );
+	( *Parser ).Request05.Value = MODBUSSwapEndian( ( *Parser ).Request05.Value );
+
+	//Check if coil is in valid range
+	if ( ( *Parser ).Request05.Coil >= MODBUSSlave.CoilCount )
+	{
+		//Illegal data address error
+		if ( ( *Parser ).Base.Address != 0 ) MODBUSBuildException( 0x05, 0x02 );
+		return;
+	}
+
+	//Check if coil value is a valid value
+	if ( ( *Parser ).Request05.Value != 0x0000 && ( *Parser ).Request05.Value != 0xFF00 )
+	{
+		//Illegal data address error
+		if ( ( *Parser ).Base.Address != 0 ) MODBUSBuildException( 0x05, 0x03 );
+		return;
+	}
+
+	MODBUSWriteMaskBit( MODBUSSlave.Coils, 1 + ( ( MODBUSSlave.CoilCount - 1 ) << 3 ), ( *Parser ).Request05.Coil, ( *Parser ).Request05.Value == 0xFF00 );
+
+	//Respond
+	MODBUSBuildResponse05( Parser );
 }

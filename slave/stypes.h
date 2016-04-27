25ca6b675ec22c57eb00c5ecc769b656127dfc1a
Jacajack <Jacajack@wp.pl>
2016-04-27 20:04:26 +0200

    Add input registers variables

--- slave/stypes.h
+++ slave/stypes.h
@@ -25,6 +25,9 @@ typedef struct
 
 	uint8_t *RegisterMask; //Masks for write protection (bit of value 1 - write protection)
 	uint16_t RegisterMaskLength; //Masks length (each mask covers 8 registers)
-	
+
+	uint16_t *InputRegisters; //Slave input registers
+	uint16_t InputRegisterCount; //Slave input count
+
 	MODBUSResponseStatus Response; //Slave response formatting status
 } MODBUSSlaveStatus; //Type containing slave device configuration data

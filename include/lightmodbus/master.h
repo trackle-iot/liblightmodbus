58d40dbc6b72194c44fefd0ab073084160543fe8
Jacek Wieczorek <mrjjot@gmail.com>
2016-08-04 11:37:36 +0200

    Add more prototypes

--- include/lightmodbus/master.h
+++ include/lightmodbus/master.h
@@ -25,6 +25,7 @@
 #define LIGHTMODBUS_MASTER_INPUT_REGISTERS 0
 #endif
 
+extern uint8_t modbusParseException( ModbusMasterStatus *status, union ModbusParser *parser );
 extern uint8_t modbusParseResponse( ModbusMasterStatus *status, uint8_t *frame, uint8_t frameLength, uint8_t *RequestFrame, uint8_t RequestFrameLength );
 extern uint8_t modbusMasterInit( ModbusMasterStatus *status );
 extern void modbusMasterEnd( ModbusMasterStatus *status ); //Free memory used by master

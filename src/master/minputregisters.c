29c6be34faaaac484d70ab37a0f57c009777a67e
Jacek Wieczorek <mrjjot@gmail.com>
2016-09-28 20:04:18 +0200

    Add license headers

--- src/master/minputregisters.c
+++ src/master/minputregisters.c
@@ -1,3 +1,23 @@
+/*
+    liblightmodbus - a lightweight, multiplatform Modbus library
+    Copyright (C) 2016  Jacek Wieczorek <mrjjot@gmail.com>
+
+    This file is part of liblightmodbus.
+
+    Liblightmodbus is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Liblightmodbus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "lightmodbus/core.h"
 #include "lightmodbus/parser.h"
 #include "lightmodbus/master/mtypes.h"
@@ -74,7 +94,7 @@ uint8_t modbusParseResponse04( ModbusMasterStatus *status, union ModbusParser *p
 		status->finished = 1;
 		return MODBUS_ERROR_ALLOC;
 	}
-	
+
 	//Copy received data to output structures array
 	for ( i = 0; i < ( parser->response04.byteCount >> 1 ); i++ )
 	{

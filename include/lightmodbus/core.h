f5f26f5d243003bddc1fe309621abcb27efa40b3
Jacek Wieczorek <mrjjot@gmail.com>
2017-05-26 23:15:27 +0200

    That newline is necessary for my sanity

--- include/lightmodbus/core.h
+++ include/lightmodbus/core.h
@@ -55,6 +55,7 @@
 #else
 #define modbusSwapEndian( data ) ( ( data << 8 ) | ( data >> 8 ) )
 #endif
+
 extern uint8_t modbusMaskRead( uint8_t *mask, uint16_t maskLength, uint16_t bit );
 extern uint8_t modbusMaskWrite( uint8_t *mask, uint16_t maskLength, uint16_t bit, uint8_t value );
 extern uint16_t modbusCRC( uint8_t *data, uint16_t length );

b575e98084badf990efc0903c2985a4ea9847017
Jacek Wieczorek <mrjjot@gmail.com>
2018-01-01 22:13:16 +0100

    Add new frame examination addon code stub

---
+++ include/lightmodbus/examine.h
@@ -0,0 +1,49 @@
+/*
+	liblightmodbus - a lightweight, multiplatform Modbus library
+	Copyright (C) 2017 Jacek Wieczorek <mrjjot@gmail.com>
+
+	This file is part of liblightmodbus.
+
+	Liblightmodbus is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Liblightmodbus is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIGHTMODBUS_EXAMINE
+#define LIGHTMODBUS_EXAMINE
+
+#include <inttypes.h>
+#include "core.h"
+#include "libconf.h"
+
+#define MODBUS_EXAMINE_REQUEST  0
+#define MODBUS_EXAMINE_RESPONSE 1
+
+typedef struct modbusFrameInfo
+{
+	uint8_t address; //Slave address
+	uint8_t function; //Function
+	uint8_t exception; //Exception number
+	uint8_t type; //Data type (coil/register and so on)
+	uint8_t index; //Register index
+	uint8_t count; //Data unit count
+	uint8_t access; //Access type - read/write
+	uint16_t crc; //CRC
+
+	//Binary data - pointer and length in bytes
+	uint8_t *data;
+	uint8_t length;
+} ModbusFrameInfo;
+
+extern uint8_t modbusExamine( ModbusFrameInfo *info, uint8_t dir, uint8_t *frame, uint8_t length );
+
+#endif
---
+++ src/examine.c
@@ -0,0 +1,71 @@
+/*
+	liblightmodbus - a lightweight, multiplatform Modbus library
+	Copyright (C) 2017 Jacek Wieczorek <mrjjot@gmail.com>
+
+	This file is part of liblightmodbus.
+
+	Liblightmodbus is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Liblightmodbus is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <lightmodbus/core.h>
+#include <lightmodbus/parser.h>
+#include <lightmodbus/examine.h>
+#include <string.h>
+#include <stddef.h>
+
+//Examines Modbus frame and returns information in ModbusFrameInfo
+//This function doesn't perform ANY data checking apart from CRC check
+//Please keep in mind, that if return value is different from MODBUS_ERROR_OK
+//data returned in the structure is worthless
+uint8_t modbusExamine( ModbusFrameInfo *info, uint8_t dir, uint8_t *frame, uint8_t length )
+{
+	union modbusParser *parser;
+
+	//Null pointer check
+	if ( info == NULL || frame == NULL || length == 0 ) return MODBUS_ERROR_OTHER;
+
+	//Initial struct clenup
+	memset( info, 0, sizeof( struct modbusFrameInfo ) );
+	info->data = NULL; //This is for weird patforms that don't consider 0 to be NULL
+
+	parser = (union modbusParser*) frame;
+
+	//CRC check (should satisfy both little and big endian platforms - see modbusCRC)
+	info->crc = modbusCRC( frame, length - 2 );
+	if ( info->crc != ( frame[length - 2] | ( frame[length - 1] << 8 ) ) ) return MODBUS_ERROR_CRC;
+
+	//Copy basic information
+	info->address = parser->base.address;
+	info->function = parser->base.function;
+
+	//Interpret exception - we don't even care about data direction
+	if ( length == 5 )
+	{
+		info->exception = parser->exception.code;
+		return MODBUS_ERROR_OK;
+	}
+
+	//TODO parse requests and responses
+	if ( dir == MODBUS_EXAMINE_REQUEST )
+	{
+
+	}
+	else if ( MODBUS_EXAMINE_RESPONSE )
+	{
+
+	}
+	else return MODBUS_ERROR_OTHER;
+
+	return MODBUS_ERROR_OK;
+}

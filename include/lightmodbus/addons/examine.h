70769661cf42b1ce1475851cb6d0747166288eee
Jacek Wieczorek <mrjjot@gmail.com>
2018-01-10 17:36:56 +0100

    Minor comment changes

--- include/lightmodbus/addons/examine.h
+++ include/lightmodbus/addons/examine.h
@@ -41,14 +41,14 @@
 
 typedef struct modbusFrameInfo
 {
-	uint8_t direction; //Just a friendly reminder
+	uint8_t direction; //Just a friendly reminder (MODBUS_EXAMINE_REQUEST/MODBUS_EXAMINE_RESPONSE)
 	uint8_t address; //Slave address
 	uint8_t function; //Function
 	uint8_t exception; //Exception number
-	uint8_t type; //Data type (coil/register and so on)
+	uint8_t type; //Data type (MODBUS_EXAMINE_COIL/MODBUS_EXAMINE_HOLDING_REGISTER etc.)
 	uint8_t index; //Register index
 	uint8_t count; //Data unit count
-	uint8_t access; //Access type - read/write
+	uint8_t access; //Access type (MODBUS_EXAMINE_READ/MODBUS_EXAMINE_WRITE)
 	uint16_t crc; //CRC
 
 	//In case of request 22

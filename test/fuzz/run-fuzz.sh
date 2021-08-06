003978e7ad580d39f52b37c44494ea16f79e7f88
Jacek Wieczorek <mrjjot@gmail.com>
2021-08-07 00:45:30 +0200

    Run master afl instance by default

--- test/fuzz/run-fuzz.sh
+++ test/fuzz/run-fuzz.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")"
 tar xvf samples.tar
 AFL_USE_ASAN=1 afl-clang++ -Wall -I../../include slavefuzz.cpp -m32 -g -O3 -DFUZZ -o slavefuzz
-AFL_SKIP_CPUFREQ=1 afl-fuzz -i samples -o out -m 800 ./slavefuzz
+AFL_SKIP_CPUFREQ=1 afl-fuzz -i samples -o out -M master -m 1024 ./slavefuzz
